b218dc9e703e1dd018e7c1021a6e20433ae17cf3
Shashi Ranjan <shashiranjankv@gmail.com>
2020-07-12 22:58:15 +0530

    Create .travis.yml
    Created kivyauth package
    
    Create requirements.txt
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml

---
+++ kivyauth/initialize.py
@@ -0,0 +1,46 @@
+from kivy.logger import Logger
+from jnius import autoclass
+from android.activity import bind as result_bind
+
+from kivyauth.callbacks import PythonFacebookCallback
+from kivyauth.listeners import GoogleActivityListener
+
+#----For Google Login----#
+Gso= autoclass('com.google.android.gms.auth.api.signin.GoogleSignInOptions')
+GsoBuilder= autoclass('com.google.android.gms.auth.api.signin.GoogleSignInOptions$Builder')
+GSignIn= autoclass('com.google.android.gms.auth.api.signin.GoogleSignIn')
+
+#----For Facebook Login----#
+CallbackManagerFactory= autoclass('com.facebook.CallbackManager$Factory')
+FacebookSdk= autoclass('com.facebook.FacebookSdk')
+LoginManager= autoclass('com.facebook.login.LoginManager')
+
+FirebaseAuth= autoclass('com.google.firebase.auth.FirebaseAuth')
+
+PythonActivity= autoclass('org.kivy.android.PythonActivity')
+
+context= PythonActivity.mActivity
+
+def initialize_google(success_listener, error_listener, RC_SIGN_IN):
+    gso= GsoBuilder(Gso.DEFAULT_SIGN_IN).requestEmail().build()
+    mGSignInClient= GSignIn.getClient(context, gso)
+    gal= GoogleActivityListener(success_listener, error_listener, RC_SIGN_IN)
+    result_bind(on_activity_result=gal.google_activity_listener)
+
+    Logger.info("KivyAuth: Initialized google signin")
+    return mGSignInClient
+
+def initialize_fb(success_listener, cancel_listener, error_listener):
+    FacebookSdk.sdkInitialize(context.getApplicationContext())
+    mCallbackManager = CallbackManagerFactory.create()
+    mFacebookCallback= PythonFacebookCallback(success_listener, cancel_listener, error_listener)
+    result_bind(on_activity_result=mCallbackManager.onActivityResult)
+
+    Logger.info("KivyAuth: Initialized facebook signin")
+    return [mCallbackManager, mFacebookCallback]
+
+def initialize_firebase():
+    mAuth= FirebaseAuth.getInstance()
+
+    Logger.info("KivyAuth: Initialized firebase auth")
+    return mAuth
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="KivyAuth",
+    version="0.1",
+    packages=["kivyauth"],
+    package_data={"kivyauth": ["*.py"],},
+    # metadata to display on PyPI
+    author="Shashi Ranjan",
+    author_email="shashiranjankv@gmail.com",
+    description="Integrate Google, Facebook, Github & Twitter login in kivy applications ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="social-login google-login facebook-login firebase-auth kivy-application kivy python",
+    url="https://github.com/shashi278/social-auth-kivy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Android",
+    ],
+    install_requires=["kivy",],
+    python_requires=">=3.6",
+)
