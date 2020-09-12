05c8250afbe52fb01f15e614b126b9e353cf52a5
shashi278 <shashiranjankv@gmail.com>
2020-09-12 23:56:26 +0530

    pycodestyle fix

--- kivyauth/__init__.py
+++ kivyauth/__init__.py
@@ -1,7 +1,8 @@
-
 from kivy.utils import platform
 from kivy.logger import Logger
 
-if platform!='android':
-    Logger.error("KivyAuth: KivyAuth currently only supports android platform.")
+if platform != "android":
+    Logger.error(
+        "KivyAuth: KivyAuth currently only supports android platform."
+        )
     exit(1)
